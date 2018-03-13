6b865cd4bcd5d982cdfb22d537fac52ecadd290b
TatianaLiholet <37273006+TatianaLiholet@users.noreply.github.com>
2018-03-13 20:31:18 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(function(){
+   $('.firstSlider').slick({
+        infinite: true,
+  		speed: 1000,
+  		fade: true,
+  		cssEase: 'linear',
+  		arrows: true,
+  		autoplay: true
+   });
+});
+
+
+$(function(){
+   $('.container_baner').slick({
+        infinite: true,
+  		speed: 1000,
+  		fade: true,
+  		cssEase: 'linear',
+  		arrows: true,
+  		autoplay: true
+   });
+});
